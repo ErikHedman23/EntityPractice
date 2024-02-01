3bc1a6722b1e586e573df034ad9aeecebbc9dc65
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-02-01 15:37:54 -0500

    Made database.  Working on adding to the database

---
+++ EntityPractice/Entities/ActorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPractice.Entities
+{
+    public class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb;" + "Trusted_Connection=True;");
+        }
+    }
+}
