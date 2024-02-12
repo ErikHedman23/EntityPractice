7cbbf70cabb68b1550e4370777a570b4a74fff72
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-02-12 16:01:26 -0500

    Seeded and presented the results in a Console.WriteLine()

--- EntityPractice/Program.cs
+++ EntityPractice/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityPractice.Entities;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+
+using (var db = new ActorDbContext()) 
+{
+    //Seed
+    db.Actors.AddRange(new Actor { Name = "Erik Hedman", Age = 24, AcademyWinner = true }, new Actor { Name = "Ben Hedman", Age = 26, AcademyWinner = true });
+
+    var count = db.SaveChanges();
+
+    //FetchData
+
+    Console.WriteLine($"{count} records added");
+
+    foreach (var Actor in db.Actors)
+    {
+        Console.WriteLine($"Name - {Actor.Name} \t\t" + $"Age: {Actor.Age}, \t\t" + $"Academy Winner: {Actor.AcademyWinner}");
+    }
+
+    Console.ReadLine();
+}
